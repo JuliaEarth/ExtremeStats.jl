280beddf85b2ec2500ca58c78711c38b32c62817
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-02-26 09:45:40 -0800

    Add samples of values from skewed distributions in the test suite

--- test/return_levels.jl
+++ test/return_levels.jl
@@ -1,14 +1,14 @@
 @testset "Return levels" begin
   srand(2018)
   x = rand(LogNormal(0, 1), 5000)
-  bmaxima = BlockMaxima(x, 50)
-  pmaxima = PeakOverThreshold(x, 5.)
+  bm = BlockMaxima(x, 50)
+  pm = PeakOverThreshold(x, 5.)
 
   if ismaintainer || istravis
     @testset "Plot recipe" begin
       function plot_return_levels(fname)
-        plt1 = returnplot(bmaxima, label="block maxima")
-        plt2 = returnplot(pmaxima, label="peak over threshold")
+        plt1 = returnplot(bm, label="block maxima")
+        plt2 = returnplot(pm, label="peak over threshold")
         plot(plt1, plt2, size=(600,800), layout=(2,1))
         png(fname)
       end
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,10 @@ istravis = "TRAVIS" ∈ keys(ENV)
 ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
 datadir = joinpath(@__DIR__,"data")
 
+# test samples
+lognorm = rand(LogNormal(2, 2), 5000)
+pareto  = rand(Pareto(.9, 10), 5000)
+
 # list of tests
 testfiles = [
   "maxima.jl",
